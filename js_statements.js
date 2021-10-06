e5e041ab0fc22f98d5d3e6561bd214b53c15c051
Divyansh Dixit <divyanshdixit96@gmail.com>
2021-10-07 01:49:45 +0530

    statements, syntax,

---
+++ js_statements.js
@@ -0,0 +1,14 @@
+console.log('statement')
+console.log('statement 2');
+
+var a = 10;
+var b = 20;
+
+var a = 10;
+var b = 20;
+
+// we can write it in one line:
+var a = 10; var b = 20;
+// if(){}else{}
+// (cond) ? '' : ''
+// js statement, js expressions
---
+++ js_syntax.js
@@ -0,0 +1,53 @@
+console.log('syntax file ');
+
+// js is case senstive language: 
+
+// console, CONSOLE
+
+var c = 10
+var C = 10;
+
+// js is loosly data type lang;
+
+
+// rule for variable name:
+
+// first letter can not be numeric.
+var $; var _d; var n1; var Nrrr; 
+
+// format for syntaxing:
+
+// 1- Declare a variable
+var r; 
+// 2- assign value to variable (define the variable)
+r = 10;
+
+var r = 10;
+// 3- computation (logics)
+
+// 10, 20 => 10 + 20 => 30 
+
+// var, let , const => to declare the vairable
+
+// var a; // declare
+
+// a = 10; // define
+
+var a = 10; //(declare and define)
+
+var b = 20;
+
+var result; 
+result = 10 + 20; // a + b
+
+// var result = a + b;
+ 
+
+console.log(result, 'helo', )
+console.log('Sum of 10 and 20 is ',result, 'adsasad' );
+
+// ' ', " "
+
+// " I'm "Divyansh" "
+
+// node 
